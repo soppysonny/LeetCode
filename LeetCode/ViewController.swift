e4d7c34c0d6a32096fc1a2b16be9db99018afd25
zhangming <zhangming@upplus.net>
2022-02-01 15:32:34 +0800

    Initial Commit

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by zhang ming on 2022/2/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
